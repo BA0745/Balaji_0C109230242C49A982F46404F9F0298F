69080d5b8668426096de262167f04cfb0a0bd8e7
BA0745 <145649680+BA0745@users.noreply.github.com>
2023-10-22 19:50:49 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,34 +1,22 @@
-# and CGPA percentage of a student
-def CgpaCalc(marks, n):
- 
-    # Variable to store the grades in
-    # every subject
-    grade = [0] * n
-   
-    # Variables to store CGPA and the
-    # sum of all the grades
-    Sum = 0
-   
-    # Computing the grades
-    for i in range(n):
-       grade[i] = (marks[i] / 10)
-   
-    # Computing the sum of grades
-    for i in range(n):
-       Sum += grade[i]
-   
-    # Computing the CGPA
-    cgpa = Sum / n
-   
-    return cgpa
-   
-# Driver code
-n = 5
-marks = [ 90, 80, 70, 80, 90 ]
- 
-cgpa = CgpaCalc(marks, n)
-       
-print("CGPA = ", '%.1f' % cgpa)
-print("CGPA Percentage = ", '%.2f' % (cgpa * 9.5))
- 
-# This code is contributed by divyeshrabadiya07￼Not
+# Define the Player class
+class Player:
+    def play(self):
+        print("The player is playing cricket")
+
+# Define the Batsman class that inherits from Player
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting")
+
+# Define the Bowler class that inherits from Player
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling")
+
+# Create objects of the Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
